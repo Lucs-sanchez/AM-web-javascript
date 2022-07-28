ef0851b684c92f17183cfd8eaaf5bb3217132e87
Lucas sanchez <Lucas.undav@gmail.com>
2022-07-27 21:07:29 -0300

    segunda entrega trabajo final

--- js/main.js
+++ js/main.js
@@ -1,72 +1,110 @@
-// PRIMERA ENTREGA TRABAJO FINAL
+let productos = [
+  {
+    nombre: `CLASSIC LOGO BLACK T-SHIRT`,
+    precio: 8250,
+    img: "../imagenes/foto1.png",
+  },
+  {
+    nombre: `CLASSIC LOGO WHITE T-SHIRT`,
+    precio: 8250,
+    img: "../imagenes/foto2.webp",
+  },
+  {
+    nombre: `OSCILLOSCOPE LOGO BLACK UNISEX T-SHIRT`,
+    precio: 8250,
+    img: "../imagenes/foto3.png",
+  },
+  {
+    nombre: `OSCILLOSCOPE LOGO WHITE UNISEX T-SHIRT`,
+    precio: 8250,
+    img: "../imagenes/foto4.png",
+  },
+  { nombre: `MONKEYS T-SHIRT`, precio: 8250, img: "../imagenes/foto5.png" },
+  {
+    nombre: `HEAVYWEIGHT CLEAR VINYL20 TRACKS GATEFOLD SLEEVE EXCLUSIVE A2 POSTER`,
+    precio: 9900,
+    img: "../imagenes/foto6.png",
+  },
+  {
+    nombre: `DOUBLE CD 20 TRACKS GATEFOLD SLEEVE`,
+    precio: 3960,
+    img: "../imagenes/foto7.png",
+  },
+  { nombre: `MONKEYS BEANIE HAT`, precio: 6650, img: "../imagenes/foto8.png" },
+];
 
-// function comprar(nombre, email, carrito) {
-//   let cant = carrito.reduce((acc, item) => item.precio + acc, 0);
-//   alert(`Felicidades ${nombre} \nEl total de tu compra es: \n $ ${cant} ARS`);
-// }
+let informacionDeUsuario = localStorage.getItem("productosEncarro");
+let productosEncarro;
 
-// class producto {
-//   constructor(id, nombre, color, categoria, precio) {
-//     this.id = id;
-//     this.nombre = nombre;
-//     this.color = color;
-//     this.categoria = categoria;
-//     this.precio = precio;
-//   }
-// }
-// let productos = [
-//   new producto(10, "Classic logo", "Negro", "ropa", 4000),
-//   new producto(11, "Oscilloscope logo", "Blanco", "ropa", 6500),
-//   new producto(12, "Monkeys", "Negro", "ropa", 5000),
-//   new producto(13, "Classic logo", "Blanco", "ropa", 6000),
-//   new producto(20, "Double cd gatefold sleeve", "Marron", "discos", 3000),
-//   new producto(21, "Vinil gategold sleeve + poster", "Marron", "discos", 7000),
-// ];
+if (!informacionDeUsuario) {
+  productosEncarro = [];
+  console.log(productosEncarro);
+} else {
+  productosEncarro = JSON.parse(informacionDeUsuario);
+  productosCarrito();
+}
 
-// let categorias = [" ropa", "discos"];
+// Funciones
 
-// let carrito = [];
-// let categoria = "";
+function meterAlCarrito(objproducto) {
+  productosEncarro.push(objproducto);
+  localStorage.setItem("productosEncarro", JSON.stringify(productosEncarro));
+  productosCarrito();
+  console.log(productosEncarro);
+}
 
-// alert("Hola! Bienvenido a la tienda de Arctic Monkeys");
+function eliminarDelCarro(nombre) {
+  productosEncarro.splice(nombre, 1);
+  localStorage.setItem("productosEncarro", JSON.stringify(productosEncarro));
+  productosCarrito();
+}
 
-// while (categoria != "salir" && categoria != null) {
-//   let arraycat = categorias.join("\n ");
-//   categoria = prompt(
-//     `Ingrese una categoria para comprar o ingrese "salir":\n\n${arraycat} `
-//   );
+function listaProductos() {
+  let aux = "";
+  for (let i = 0; i < productos.length; i++) {
+    aux =
+      aux +
+      ` <div class="container__imagen grid text-center">
+      <div class="container__imagen col">
+          <img src=${productos[i].img} class="img-fluid">
+      <div class="capa">
+          <p>${productos[i].nombre}</p>
+          <p class="capa_precio">$ ${productos[i].precio}</p>
+          <a onclick="meterAlCarrito({nombre: '${productos[i].nombre}', precio: '${productos[i].precio}'})" class="css-button-sliding-to-bottom--black">Agregar al carro</a>
+          </div>
+      </div>
+  </div>`;
+  }
+  document.getElementById("div-productos").innerHTML = aux;
+}
+listaProductos();
 
-//   if (categoria != "salir" && categoria != null) {
-//     let filtroCategoria = productos.filter(
-//       (item) => item.categoria == categoria
-//     );
-//     console.log(filtroCategoria);
+function productosCarrito() {
+  let aux = "";
+  for (let i = 0; i < productosEncarro.length; i++) {
+    aux =
+      aux +
+      ` <div class="container__productoscarro">
+            <div class="nombre__producto">
+            <p>${productosEncarro[i].nombre}</p>
+            </div>
+            <div class="precio__producto">
+            <p>$ ${productosEncarro[i].precio} ARS</p>
+            </div>
+            <div class="tacho__producto">
+            <a onclick="eliminarDelCarro(${i})" ><i class="fa-solid fa-trash-can"></i></a>
+            </div>
+        </div>`;
+  }
+  document.getElementById("div-carrito").innerHTML = aux;
+}
 
-//     let cartel = "";
-//     for (let i = 0; i < filtroCategoria.length; i++) {
-//       cartel += ` id: ${filtroCategoria[i].id} \n
-//         nombre: ${filtroCategoria[i].nombre}
-//         precio: ${filtroCategoria[i].precio}
-//         \n`;
-//     }
-//     let idSeleccionado = parseInt(
-//       prompt(
-//         `"Seleccione el ID del producto que quiere comprar: \n\n ${cartel}`
-//       )
-//     );
-//     let productoParaCarro = filtroCategoria.find(
-//       (item) => item.id == idSeleccionado
-//     );
-//     if (productoParaCarro) {
-//       carrito.push(productoParaCarro);
-//       console.log(carrito);
-//     }
-//   }
-// }
+function finalCompra() {
+  let compraProductos = document.getElementById("finalizarCompra");
 
-// if (carrito.length > 0) {
-//   alert("Ingrese sus datos para terminar la compra");
-//   let nombre = prompt("Ingrese su nombre");
-//   let email = prompt("Ingrese su email");
-//   comprar(nombre, email, carrito);
-// }
+  finalizarCompra = `<div>
+  <a class="css-button-sliding-to-bottom--black">Finalizar compra</a>
+ </div>`;
+
+  compraProductos.innerHTML = finalizarCompra;
+}
